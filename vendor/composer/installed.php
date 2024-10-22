85c00e7d37e6ad8122b8a276c5b30660d0d9f27d
Suraj <suraj@epravidi.com>
2024-10-22 22:58:05 +0545

    Files added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'creativetimofficial/material-dashboard',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '570aa19b02b5d0782fb375a5aae74c331f283c0b',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'creativetimofficial/material-dashboard' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '570aa19b02b5d0782fb375a5aae74c331f283c0b',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
