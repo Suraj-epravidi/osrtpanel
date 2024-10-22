26771e83f0ecbe6be8090e9945c0b0109ee5f9df
Suraj <suraj@epravidi.com>
2024-10-22 22:48:05 +0545

    Fixed tables.php

--- pages/tables.php
+++ pages/tables.php
@@ -1,17 +1,4 @@
-<!--
-=========================================================
-* Material Dashboard 2 - v3.0.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -200,7 +187,6 @@ $dbname = "epravidi_osrt";       // Replace with your database name
 
 // Create connection
 $conn = new mysqli($host, $username, $password, $dbname);
-        print_r($conn);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
