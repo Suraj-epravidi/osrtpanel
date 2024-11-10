dd91fd7a07d82eedd99776a888cf7044762d7d3b
ego-zxx <surajthapa2006@gmail.com>
2024-11-10 23:43:47 +0545

    updated

--- index.php
+++ index.php
@@ -379,77 +379,6 @@ $pdo = null;
             </div>
           </div>
         </div>
-        <div class="row rowStatus">
-        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4 boxStatus">
-            <div class="card" >
-              <div class="card-header p-3 pt-2">
-                <div
-                  class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute"
-                >
-                  <i class="material-icons opacity-10">person</i>
-                </div>
-                <div class="text-end pt-1">
-                  <p class="text-sm mb-0 text-capitalize">Reviews</p>
-                  <h4 class="mb-0"><?php echo $totalReviews;?></h4>
-                </div>
-              </div>
-              <hr class="dark horizontal my-0" />
-              <div class="card-footer p-3">
-                <p class="mb-0">
-                  <span class="text-success text-sm font-weight-bolder">
-</span>
-                 
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4 boxStatus">
-            <div class="card">
-              <div class="card-header p-3 pt-2">
-                <div
-                  class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute"
-                >
-                  <i class="material-icons opacity-10">person</i>
-                </div>
-                <div class="text-end pt-1">
-                  <p class="text-sm mb-0 text-capitalize">Brands</p>
-                  <h4 class="mb-0"><?php echo $totalBrands;?></h4>
-                </div>
-              </div>
-              <hr class="dark horizontal my-0" />
-              <div class="card-footer p-3">
-                <p class="mb-0">
-                  <span class="text-success text-sm font-weight-bolder"
-                    > </span
-                  >
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4 boxStatus">
-            <div class="card">
-              <div class="card-header p-3 pt-2">
-                <div
-                  class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute"
-                >
-                  <i class="material-icons opacity-10">person</i>
-                </div>
-                <div class="text-end pt-1">
-                  <p class="text-sm mb-0 text-capitalize">Categories</p>
-                  <h4 class="mb-0"><?php echo $totalCategories;?></h4>
-                </div>
-              </div>
-              <hr class="dark horizontal my-0" />
-              <div class="card-footer p-3">
-                <p class="mb-0">
-                  <span class="text-success text-sm font-weight-bolder"
-                    > </span
-                  >
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
         <!-- <div class="row mt-4">
           <div class="col-lg-4 col-md-6 mt-4 mb-4">
             <div class="card z-index-2">
@@ -540,6 +469,79 @@ $pdo = null;
           </div>
         </div> -->
       </div>
+      <div class="container-fluid py-4">
+      <div class="row rowStatus">
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4 boxStatus">
+            <div class="card" >
+              <div class="card-header p-3 pt-2">
+                <div
+                  class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute"
+                >
+                  <i class="material-icons opacity-10">person</i>
+                </div>
+                <div class="text-end pt-1">
+                  <p class="text-sm mb-0 text-capitalize">Reviews</p>
+                  <h4 class="mb-0"><?php echo $totalReviews;?></h4>
+                </div>
+              </div>
+              <hr class="dark horizontal my-0" />
+              <div class="card-footer p-3">
+                <p class="mb-0">
+                  <span class="text-success text-sm font-weight-bolder">
+</span>
+                 
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4 boxStatus">
+            <div class="card">
+              <div class="card-header p-3 pt-2">
+                <div
+                  class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute"
+                >
+                  <i class="material-icons opacity-10">person</i>
+                </div>
+                <div class="text-end pt-1">
+                  <p class="text-sm mb-0 text-capitalize">Brands</p>
+                  <h4 class="mb-0"><?php echo $totalBrands;?></h4>
+                </div>
+              </div>
+              <hr class="dark horizontal my-0" />
+              <div class="card-footer p-3">
+                <p class="mb-0">
+                  <span class="text-success text-sm font-weight-bolder"
+                    > </span
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4 boxStatus">
+            <div class="card">
+              <div class="card-header p-3 pt-2">
+                <div
+                  class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute"
+                >
+                  <i class="material-icons opacity-10">person</i>
+                </div>
+                <div class="text-end pt-1">
+                  <p class="text-sm mb-0 text-capitalize">Categories</p>
+                  <h4 class="mb-0"><?php echo $totalCategories;?></h4>
+                </div>
+              </div>
+              <hr class="dark horizontal my-0" />
+              <div class="card-footer p-3">
+                <p class="mb-0">
+                  <span class="text-success text-sm font-weight-bolder"
+                    > </span
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
     </main>
 
     <!--   Core JS Files   -->
