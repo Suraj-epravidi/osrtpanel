fdf88470ffe3d6f2d086d10a45e2678d22a5bc56
Rockyarch <rocky@epravidi.com>
2024-11-14 23:53:57 +0530

    Checking products downloads

--- pages/products.php
+++ pages/products.php
@@ -421,8 +421,13 @@ $conn->close();
 <!-- HTML for displaying the products -->
 
 <div class="container-fluid py-4">
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addReviewModal" onclick="redirectDownload()">
-Download
+<script>
+  function redirectDownload(){
+    window.location.href = "./product_download.php";
+  }
+</script>
+<button type="button" class="btn btn-primary" onclick="redirectDownload()">
+Do
 </button>
   <div class="row">
     <div class="col-12">
