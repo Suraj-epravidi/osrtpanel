460daf30f76f7682621ba78f611afc6f9f36f90a
ShreyanshPokharel <shreyansh@epravidi.com>
2024-12-25 22:01:43 +0545

    import file

--- pages/products.php
+++ pages/products.php
@@ -422,6 +422,68 @@ Generate All description
 
 <p class="opacity-5 text-dark">AI User Credit: <br>
 
+<?php
+if (isset($_POST['import'])) {
+    // Database connection
+    $servername = "192.250.235.20";
+    $username = "epravidi_osrt_data";
+    $password = "UQ!r.gTOz=oo";
+    $dbname = "epravidi_osrt";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Check if a file is uploaded
+    if (isset($_FILES['excel']['tmp_name']) && $_FILES['excel']['tmp_name'] !== '') {
+        $file = $_FILES['excel']['tmp_name'];
+        
+        // Open the file in read mode
+        if (($handle = fopen($file, "r")) !== false) {
+            // Skip the first row if it contains headers
+            $isFirstRow = true;
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                if ($isFirstRow) {
+                    $isFirstRow = false;
+                    continue; 
+                }
+
+                // Extract data from each row (adjust indices based on your CSV format)
+                $product_id = $conn->real_escape_string($data[0]);
+                $product_name = $conn->real_escape_string($data[1]);
+                $description = $conn->real_escape_string($data[2]); 
+                $product_code = $conn->real_escape_string($data[3]); 
+                $color = $conn->real_escape_string($data[4]); 
+                $brand = $conn->real_escape_string($data[5]); 
+                $material = $conn->real_escape_string($data[6]); 
+                $category = $conn->real_escape_string($data[7]); 
+                $price = $conn->real_escape_string($data[8]); 
+                $stock = $conn->real_escape_string($data[9]); 
+                $dimensions = $conn->real_escape_string($data[10]); 
+                
+
+                // Insert data into the database
+                $query = "INSERT INTO products (product_id, product_name, description, product_code, color, brand, material, category, price, stock, image, dimensions, created at) VALUES ('$product_id', '$product_name', '$description', '$product_code', '$color', '$brand', '$material', '$category', '$price', '$stock', NULL, '$dimensions', NOW())";
+
+
+                if (!$conn->query($query)) {
+                    echo "Error: " . $conn->error . "<br>";
+                }
+            }
+
+            fclose($handle);
+            echo "<script>alert('File imported successfully!');</script>";
+        } else {
+            echo "<script>alert('Failed to open the file.');</script>";
+        }
+    } else {
+        echo "<script>alert('Please upload a file.');</script>";
+    }
+  }
+    $conn->close();
+?>
 
 <div class="modal fade" id="importReviewModal" tabindex="-1" aria-labelledby="importReviewModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -439,45 +501,7 @@ Generate All description
   </div>
 </div>
 
-<?php
 
-  if(isset($_POST["import"])){
-
-  $fileName = $_FILES["excel"]["name"];
-
-  $fileExtension = explode('.', $fileName);
-
-  $fileExtension = strtolower(end($fileExtension));
-
-  $newFileName = date("Y.m.d") . " - " . date("h.i.sa") . "." . $fileExtension;
-
-  $targetDirectory = "uploads/" . $newFileName;
-
-  move_uploaded_file($_FILES["excel"]["tmp_name"], $targetDirectory);
-
-  error_reporting(0);
-
-
-  require "excelReader/excel_reader2.php";
-  require "excelReader/SpreadsheetReader.php";
-
- $reader  = new SpreadsheetReader($targetDirectory);
- foreach($reader as $key => $row){
- 	$product_name = $row[0];
-	$description = $row[1];
-	$product_code = $row[2];
-	$color = $row[3];
-	$brand = $row[4];
-	$material= $row[5];
-	$category = $row[6];
-	$price = $row[7];
-	$stock = $row[8];
-	$dimensions = $row[9];
-	
-	mysqli_query($conn, "INSERT INTO products VALUES('', '$product_name', '$description', '$product_code, '$color', '$brand', '$material', '$category', '$price', '$stock', '$dimensions')");
-}
-  }
-?>
 
   <div class="row">
     <div class="col-12">
