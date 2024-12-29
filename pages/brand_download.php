d183f0f45e8baf8711dca457bd39289d1c175e30
Suraj-epravidi <suraj@epravidi.com>
2024-12-29 10:27:24 +0545

    Changes again

--- pages/brand_download.php
+++ pages/brand_download.php
@@ -5,6 +5,7 @@ $username = "osrtraders_epravidi";
 $password = "UQ!r.gTOz=oo";
 $dbname = "osrtraders_epravidi_osrt";
 
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
